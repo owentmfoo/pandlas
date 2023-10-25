e96c223d58ade9299ff844f5340f1144a13eaca0
Owen Foo <owen.foo@mclaren.com>
2023-10-25 12:04:03 +0100

    update timestamp2long to return int64 instead

--- Pandlas/pandlas/utils.py
+++ Pandlas/pandlas/utils.py
@@ -6,7 +6,7 @@ import pandas as pd
 
 
 def timestamp2long(timestamp: Union[pd.Timestamp, pd.DatetimeIndex], start_date: pd.Timestamp = None) -> Union[
-    pd.Index, float]:
+    pd.Index, int]:
     """Convert timestamps to ns from the midnight of start date.
 
     Args:
@@ -35,7 +35,13 @@ def timestamp2long(timestamp: Union[pd.Timestamp, pd.DatetimeIndex], start_date:
     ddays = (timestamp - start_date.floor('D')).days
     ns_in_day = (timestamp.hour * 3600 + timestamp.minute * 60 + timestamp.second) * 1e9 + timestamp.microsecond * 1e3 + timestamp.nanosecond
     long = ns_in_day + ddays * 24 * 3600 * 1e9
+
+    if isinstance(timestamp, pd.Timestamp):
+        long = np.int64(long)
+    else:
+        long = long.astype(np.int64)
+
     if np.array(long > 2 ** 63).max():
-        logging.exception("S")
+        logging.error("Timestamp is too large to be represented by long.")
         raise OverflowError("Timestamp is too large to be represented by long")
     return long
