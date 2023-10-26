7e1c3b61d96feb18dc926e116680f6f69a0e35e0
Owen Foo <owen.foo@mclaren.com>
2023-10-26 15:36:20 +0100

    Add long2timestamp

--- Pandlas/pandlas/utils.py
+++ Pandlas/pandlas/utils.py
@@ -5,8 +5,9 @@ import numpy as np
 import pandas as pd
 
 
-def timestamp2long(timestamp: Union[pd.Timestamp, pd.DatetimeIndex], start_date: pd.Timestamp = None) -> Union[
-    pd.Index, int]:
+def timestamp2long(
+    timestamp: Union[pd.Timestamp, pd.DatetimeIndex], start_date: pd.Timestamp = None
+) -> Union[pd.Index, int]:
     """Convert timestamps to ns from the midnight of start date.
 
     Args:
@@ -15,7 +16,7 @@ def timestamp2long(timestamp: Union[pd.Timestamp, pd.DatetimeIndex], start_date:
         first day. The timestamp will be rounded down to the nearest day and the ns outputted will be counted from midnight.
 
     Returns:
-        List of floats representing ns passed since midnight of start date.
+        Array of int64 representing ns passed since midnight of start date.
 
     Raises:
         OverflowError: If the output floats are larger than a C# long can handle
@@ -27,14 +28,17 @@ def timestamp2long(timestamp: Union[pd.Timestamp, pd.DatetimeIndex], start_date:
         elif isinstance(timestamp, pd.DatetimeIndex):
             start_date = timestamp[0]
         else:
-            logging.warning('Incorrect input type for timestamp at timestamp2long')
+            logging.warning("Incorrect input type for timestamp at timestamp2long")
             try:
                 start_date = timestamp[0]
             except IndexError:
                 start_date = timestamp
-    ddays = (timestamp - start_date.floor('D')).days
+    ddays = (timestamp - start_date.floor("D")).days
     ns_in_day = (
-                        timestamp.hour * 3600 + timestamp.minute * 60 + timestamp.second) * 1e9 + timestamp.microsecond * 1e3 + timestamp.nanosecond
+        (timestamp.hour * 3600 + timestamp.minute * 60 + timestamp.second) * 1e9
+        + timestamp.microsecond * 1e3
+        + timestamp.nanosecond
+    )
     long = ns_in_day + ddays * 24 * 3600 * 1e9
 
     if isinstance(timestamp, pd.Timestamp):
@@ -42,14 +46,27 @@ def timestamp2long(timestamp: Union[pd.Timestamp, pd.DatetimeIndex], start_date:
     else:
         long = long.astype(np.int64)
 
-    if np.array(long > 2 ** 63).max():
+    if np.array(long > 2**63).max():
         logging.error("Timestamp is too large to be represented by long.")
         raise OverflowError("Timestamp is too large to be represented by long")
     return long
 
 
-def long2timestamp(long: np.ndarray, start_date: Union[pd.Timestamp, np.datetime64]) -> np.ndarray:
-    """Converts ATLAS timestamp in int64 ns from midnight to pandas timestamp."""
-    # TODO: Add test.
-    time_in_day = long.astype('timedelta64[ns]')
-    return np.datetime64(start_date)+time_in_day
+def long2timestamp(
+    long: Union[pd.Index, int], start_date: Union[pd.Timestamp, np.datetime64]
+) -> pd.Timestamp:
+    """Converts ATLAS timestamp in int64 ns from midnight to pandas timestamp.
+
+    Args:
+        long: Array of in64 representing ns passed since midnight of start date.
+        start_date: The date to start counting from.
+
+    Returns:
+        Series of pandas Timestamp
+    """
+    time_in_day = long.astype("timedelta64[ns]")
+    if isinstance(start_date, pd.Timestamp):
+        start_date = start_date.to_numpy().astype("datetime64[D]")
+    elif isinstance(start_date, np.datetime64):
+        start_date = start_date.astype("datetime64[D]")
+    return pd.to_datetime(start_date + time_in_day)
