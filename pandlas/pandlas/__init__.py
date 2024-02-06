74004c57270e8a5eb5c0fe177aa015131a68ca1a
Owen Foo <owen.foo@mclaren.com>
2024-02-06 10:09:14 +0000

    Edit dcostrings

--- pandlas/pandlas/__init__.py
+++ pandlas/pandlas/__init__.py
@@ -1,5 +1,10 @@
+"""Pandlas
+
+An example package demonstrating how to use the SQLRace API. This package is not maintained nor officially supported.
+"""
 import os
 import clr  # Pythonnet
 import numpy as np
 
 from pandlas.session_frame import SessionFrame
+from pandlas.SqlRace import SQLRaceDBConnection,SQLiteConnection,Ssn2Session
