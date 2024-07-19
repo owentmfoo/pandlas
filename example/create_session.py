d250d0ec0a9e7ad8448aeaca243b3b1420e84916
Owen Foo <owen.foo@mclarenapplied.com>
2024-07-20 00:22:34 +0100

    linting, docstring for examples

--- example/create_session.py
+++ example/create_session.py
@@ -1,17 +1,18 @@
 """
 Example to create a session with multiple data rate and merging configs
 """
+
 import datetime
 import logging
 import pandas as pd
 import numpy as np
 
+from pandlas import SQLiteConnection
 import pandlas.SqlRace as sr
-import pandlas.session_frame
 
 logging.basicConfig(level=logging.INFO)
 
-SQLITE_DB_DIR = r'C:\McLaren Applied\pandlas\ExampleSessions.ssndb'
+SQLITE_DB_DIR = r"C:\McLaren Applied\pandlas\ExampleSessions.ssndb"
 
 start = pd.Timestamp("now")
 
@@ -19,7 +20,7 @@ start = pd.Timestamp("now")
 #   - dataframe with a datetime index
 #   - a column with data in doubles
 df = pd.DataFrame()
-df.index = pd.date_range(start, periods=1000, freq='S')
+df.index = pd.date_range(start, periods=1000, freq="S")
 df.loc[:, "Param 1"] = np.sin(np.linspace(0, 10 * np.pi, num=1000))
 
 # some optional extras
@@ -27,18 +28,25 @@ df.loc[:, "Param 1"] = np.sin(np.linspace(0, 10 * np.pi, num=1000))
 #   - change the parameter group name
 #   - disable the progress bars
 df2 = pd.DataFrame()
-df2.index = pd.date_range(start, periods=100, freq='10S')
+df2.index = pd.date_range(start, periods=100, freq="10S")
 df2.loc[:, "Param 2"] = np.sin(np.linspace(0, 10 * np.pi, num=100))
 df2.atlas.ParameterGroupIdentifier = "Sub group 1"
 df2.atlas.ApplicationGroupName = "AppGroup2"
-df2.atlas.descriptions = {"Param 2:AppGroup2":"Custom Description"}
-df2.atlas.display_format = {"Param 2:AppGroup2":"%5.2f"}
-df2.atlas.units = {"Param 2:AppGroup2":"m/s"}
+df2.atlas.descriptions = {"Param 2:AppGroup2": "Custom Description"}
+df2.atlas.display_format = {"Param 2:AppGroup2": "%5.2f"}
+df2.atlas.units = {"Param 2:AppGroup2": "m/s"}
 
-session_identifier = f"TestSession - {datetime.datetime.now().strftime('%y/%m/%d %H:%M:%S')}"
-with sr.SQLiteConnection(SQLITE_DB_DIR, session_identifier, mode='w') as session:
+session_identifier = (
+    f"TestSession - {datetime.datetime.now().strftime('%y/%m/%d %H:%M:%S')}"
+)
+with SQLiteConnection(SQLITE_DB_DIR, session_identifier, mode="w") as session:
     df.atlas.to_atlas_session(session)
     df2.atlas.to_atlas_session(session, show_progress_bar=False)
-    sr.add_lap(session, start+pd.Timedelta(500,'S'), 2, "Lap 2", True)
-    sr.add_point_marker(session, start+pd.Timedelta(100,'S'), "Example Point Marker")
-    sr.add_range_marker(session, start+pd.Timedelta(30,'S'), start+pd.Timedelta(90,'S'), "Example Range Marker")
+    sr.add_lap(session, start + pd.Timedelta(500, "S"), 2, "Lap 2", True)
+    sr.add_point_marker(session, start + pd.Timedelta(100, "S"), "Example Point Marker")
+    sr.add_range_marker(
+        session,
+        start + pd.Timedelta(30, "S"),
+        start + pd.Timedelta(90, "S"),
+        "Example Range Marker",
+    )
